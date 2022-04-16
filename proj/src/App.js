4f33f6b5f434c7fa484f710b6d58122ae1d59dff
achintyapro03 <achintyaharsha03@gmail.com>
2022-04-16 16:59:48 +0530

    commit1

---
+++ proj/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import Nav from './components/Nav';
+
+function App() {
+  const tabs = [
+  {
+    id:1,
+    name: "Home"
+  },
+  {
+    id:2,
+    name: "About"
+  },
+  {
+    id:3,
+    name: "Logout"
+  },
+  ]
+  return (
+    <div className="App">
+      <Nav tabs={tabs}/>
+    </div>
+  );
+}
+
+export default App;
